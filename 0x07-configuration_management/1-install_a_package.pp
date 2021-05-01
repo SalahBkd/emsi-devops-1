7381a204ca4aa3edf8be04ad760932994a43056e
Ubuntu <ubuntu@gc-3118-lb-01-1617750847.c.hbtn-students-instances.internal>
2021-05-01 11:50:04 +0000

    add 1-install_a_package.pp

--- 0x07-configuration_management/1-install_a_package.pp
+++ 0x07-configuration_management/1-install_a_package.pp
@@ -2,8 +2,6 @@
 exec { 'apt-update':
   command => '/usr/bin/apt-get update',
 }
-
 package { 'puppet-lint':
-  install_options => ['-v','2.1.1'],
-  ensure          => 'installed',
+  ensure => '2.1.1',
 }
