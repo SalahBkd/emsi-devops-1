e03e06f7db32710f4ca634bf2e51022ab21b1a46
OussamaAlaoui <ousalis99@gmail.com>
2021-05-02 19:49:34 +0000

    fix 0-strace_is_your_friend.pp

--- 0x0B-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x0B-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-#debuging Apache server
+# debuging Apache server
 exec { 'fix-wordpress':
   command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
-  path => "/bin:/sbin:/usr/bin:/usr/sbin" 
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+
 }  
