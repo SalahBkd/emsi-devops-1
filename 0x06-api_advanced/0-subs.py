7dc285b30330faf62f29dda5ddfa2d2142518c30
OussamaAlaoui <ousalis99@gmail.com>
2021-05-06 17:51:08 +0000

    fix -subs.py

--- 0x06-api_advanced/0-subs.py
+++ 0x06-api_advanced/0-subs.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
-0-subs
+0-subs : using reddit api to get number of subs for a specific subreddit
 """
 import requests
 def number_of_subscribers(subreddit):
-	url = f"https://www.reddit.com/r/{subreddit}/about.json"
+	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 	headers = requests.utils.default_headers()
 	headers.update({'User-Agent': 'me'})
 	response = requests.get(url, headers=headers).json()
 	subs = response.get('data', {}).get('subscribers')
 	if subs : 
-		return subs   
-	else :
-		return 0
+		return subs 
+	return 0  
