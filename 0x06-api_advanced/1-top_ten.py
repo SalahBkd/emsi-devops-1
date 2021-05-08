19913e0636ed41510a10f623ad5721b80caa9bfa
OussamaAlaoui <ousalis99@gmail.com>
2021-05-08 08:38:43 +0000

    fix 1-top

--- 0x06-api_advanced/1-top_ten.py
+++ 0x06-api_advanced/1-top_ten.py
@@ -14,10 +14,8 @@ def top_ten(subreddit):
     headers = requests.utils.default_headers()
     headers.update({'User-Agent':'me'})
     response= requests.get(url,headers=headers,allow_redirects=False).json()
-    sub_titles=response['data']['children']
-    if response['data']['dist']!=0 :    
-        for i in sub_titles:
-            print(i['data']['title'])
-    else:
-        print('None')
-	
+    sub_titles=response.get('data', {}).get('children', [])
+    if not sub_titles : 
+        print(None)
+    for i in sub_titles:
+        print(i.get('data').get('title'))
