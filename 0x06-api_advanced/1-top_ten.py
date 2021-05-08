4d5de2fbadd421159986578ddb67f9b396b478c3
OussamaAlaoui <ousalis99@gmail.com>
2021-05-08 08:18:29 +0000

    add 1

--- 0x06-api_advanced/1-top_ten.py
+++ 0x06-api_advanced/1-top_ten.py
@@ -1,15 +1,16 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 """
 1-top_ten
 
 """
 import requests
+
 def top_ten(subreddit):
     """
     printing the top hot posts of a subreddit.
     
     """
-    url = "https://www.reddit.com/r/{}/top.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = requests.utils.default_headers()
     headers.update({'User-Agent':'me'})
     response= requests.get(url,headers=headers,allow_redirects=False).json()
