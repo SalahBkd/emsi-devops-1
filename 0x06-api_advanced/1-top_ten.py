911d836a2c7fa0a85299f19f09d08a80ca304563
OussamaAlaoui <ousalis99@gmail.com>
2021-05-08 08:20:59 +0000

    add 1

--- 0x06-api_advanced/1-top_ten.py
+++ 0x06-api_advanced/1-top_ten.py
@@ -10,13 +10,14 @@ def top_ten(subreddit):
     printing the top hot posts of a subreddit.
     
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = requests.utils.default_headers()
     headers.update({'User-Agent':'me'})
     response= requests.get(url,headers=headers,allow_redirects=False).json()
+    sub_titles=response['data']['children'][i]['data']['title']
     if response['data']['dist']!=0 :    
-        for i in range(10):
-            print(response['data']['children'][i]['data']['title'])
+        for i in sub_titles:
+            print(i)
     else:
         print('None')
 	
