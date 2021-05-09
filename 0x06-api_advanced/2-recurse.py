bacd35ffd4dcf11ca7f684d3964b4fe907a2275a
OussamaAlaoui <ousalis99@gmail.com>
2021-05-09 10:09:28 +0000

    fix

--- 0x06-api_advanced/2-recurse.py
+++ 0x06-api_advanced/2-recurse.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
 """
-return the list of all hot posts of a subreddit
+Return the list of all hot posts of a subreddit
 """
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="done"):
     """
-    the recurse function is recursive function that returs the title
+        thi is a recursive function 
+        returns all hot posts 
     """
+    # seeting user agent
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+
+    # alter url with the new value of after
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     if after != "done":
         url = url + "?after={}".format(after)
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'me'})
     response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # adding the newly added top posts 
     sub_titles = response.json().get('data', {}).get('children', [])
     if not sub_titles:
         return None
-    else:
-        # adding all the current fetched titles
-        for i in sub_titles:
-            hot_list.append(i.get('data').get('title'))
+    for i in sub_titles:
+        hot_list.append(i.get('data').get('title'))
+
+    # new value after
     after = response.json().get('data').get('after')
     if not after:
         return hot_list
-    return recurse(subreddit, hot_list, after)
-
+    return (recurse(subreddit, hot_list, after))
