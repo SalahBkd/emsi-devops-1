87fd5ce33aad14e1c77e7149fc04246de8b644d2
OussamaAlaoui <ousalis99@gmail.com>
2021-05-09 09:42:51 +0000

    fix 0

--- 0x06-api_advanced/0-subs.py
+++ 0x06-api_advanced/0-subs.py
@@ -6,14 +6,17 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """ function to get the number of subs"""
+    """ 
+    function to get the number of subs
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
+    
     # getting response
     response = requests.get(url, headers=headers, allow_redirects=False).json()
     subs = response.get('data', {}).get('subscribers')
     if not subs:
         return 0
     return subs
- 
