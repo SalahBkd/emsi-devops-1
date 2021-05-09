a245fd7d76f24fea089c4a45be01bd79d336d3b3
OussamaAlaoui <ousalis99@gmail.com>
2021-05-09 07:42:53 +0000

    add 2-recurse.py

--- 0x06-api_advanced/0-subs.py
+++ 0x06-api_advanced/0-subs.py
@@ -3,15 +3,19 @@
 0-subs : using reddit api to get number of subs for a specific subreddit
 """
 import requests
+
+
+
+
 def number_of_subscribers(subreddit):
     """ function to get the number of subs"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'me'})
-    # getting response 
+    # getting response
     response = requests.get(url, headers=headers,allow_redirects=False).json()
     subs = response.get('data', {}).get('subscribers')
-    if not subs : 
+    if not subs :
         return 0 
     return subs
  
