9fb833f463df331090b895a8a985d944a4db1280
OussamaAlaoui <ousalis99@gmail.com>
2021-05-09 09:53:53 +0000

    fix conflict

--- 0x06-api_advanced/1-top_ten.py
+++ 0x06-api_advanced/1-top_ten.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """
-1-top_ten
+1-top_ten getting top 10 hot posts
 """
 import requests
 
 
 def top_ten(subreddit):
     """
-    printing the top hot posts of a subreddit.
+        printing the top hot posts of a subreddit.
     """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    # setting user agent
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'me'})
+    
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False).json()
     sub_titles = response.get('data', {}).get('children', [])
     # test if we got some info from the subreddit
@@ -19,4 +21,3 @@ def top_ten(subreddit):
         print(None)
     for i in sub_titles:
         print(i.get('data').get('title'))
-
