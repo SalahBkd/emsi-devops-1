8262cff27ccd65f83650d0d07f4c262e99763840
OussamaAlaoui <ousalis99@gmail.com>
2021-05-09 10:46:01 +0000

    add  0-the_sky_is_the_limit_not.pp and 1-user_limit.pp

---
+++ 0x0C-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# fixing  1057 requests failed
+
+exec {'fix request error':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
