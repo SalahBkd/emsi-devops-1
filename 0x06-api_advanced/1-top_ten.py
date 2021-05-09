893ecf5978337f6496a243d7e61dfeb4d7035494
OussamaAlaoui <ousalis99@gmail.com>
2021-05-09 09:55:25 +0000

    fix

--- 0x06-api_advanced/1-top_ten.py
+++ 0x06-api_advanced/1-top_ten.py
@@ -11,12 +11,11 @@ def top_ten(subreddit):
     """
     # setting user agent
     headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'me'})
+    headers.update({'User-Agent': 'My User Agent 1.0'})
     
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False).json()
     sub_titles = response.get('data', {}).get('children', [])
-    # test if we got some info from the subreddit
     if not sub_titles:
         print(None)
     for i in sub_titles:
