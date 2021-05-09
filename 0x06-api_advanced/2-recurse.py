2bf09e2b3cf1e96ded059f77b12311d67baafe99
OussamaAlaoui <ousalis99@gmail.com>
2021-05-09 08:29:16 +0000

    fix 2-recurse.py

--- 0x06-api_advanced/2-recurse.py
+++ 0x06-api_advanced/2-recurse.py
@@ -2,30 +2,28 @@
 """
 return the list of all hot posts of a subreddit
 """
-import requests
-def recurse(subreddit, hot_list=[], count=0):
+import requests 
+
+
+
+def recurse(subreddit, hot_list=[], after="done"):
     """
-    the recurse function is recursive function that returs the title of all hot posts in subreddit
+    the recurse function is recursive function that returs the title of all hot posts in subreddit 
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if after != "done":
+        url = url + "?after={}".format(after)
     headers = requests.utils.default_headers()
     headers.update({'User-Agent':'me'})
-    response= requests.get(url,headers=headers,allow_redirects=False).json()
-    sub_titles=response.get('data', {}).get('children', [])
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    sub_titles=response.json().get('data', {}).get('children', [])
     if not sub_titles:
-        return None
-    if len(sub_titles)==count:
         return hot_list
-    if not hot_list:
-        hot_list.append(sub_titles[-1].get('data').get('title'))
-        del sub_titles[-1]
-        count +=1
-        return recurse(subreddit,hot_list,count)
-    else : 
-        # to remove all the previous titles from sub_titles
-        for i in range(count):
-            del sub_titles[-1]
-        hot_list.append(sub_titles[-1].get('data').get('title'))
-        del sub_titles[-1]
-        count +=1
-        return recurse(subreddit,hot_list,count)
+    else :
+        # adding all the current fetched titles
+        for i in sub_titles:
+            hot_list.append(i.get('data').get('title'))
+    after = response.json().get('data').get('after')
+    if not after:
+        return hot_list
+    return recurse(subreddit,hot_list,after)
