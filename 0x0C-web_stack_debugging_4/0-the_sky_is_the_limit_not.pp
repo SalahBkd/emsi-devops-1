e9dbb94cb3865a99bc03bcd32d29569083d85251
OussamaAlaoui <ousalis99@gmail.com>
2021-05-09 10:38:53 +0000

    add 1-user_limit.pp

--- 0x0C-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x0C-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fixing  1057 requests failed
 
-exec {'modify max open files limit setting':                                                                                                                                                                
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',                                                                                                                       
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',                                                                                                    
+exec {'modify max open files limit setting': 
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart', 
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games', 
 }
